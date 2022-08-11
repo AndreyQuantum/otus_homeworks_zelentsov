63386f2de00c87e35ad969d527aeddb505a40120
zelen <zelentsov1254@gmail.com>
2022-08-11 04:23:58 -0700

    completed hw8

---
+++ homework_08/clothes_shop/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DJANGO_APPS=("shopapp")
+for app in ${DJANGO_APPS[@]}; do
+  python manage.py makemigrations $app
+done
+python manage.py makemigrations
+python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD=administrator
+export DJANGO_SUPERUSER_PASSWORD
+DJANGO_SUPERUSER_EMAIL=administrator@admin.com
+DJANGO_SUPERUSER_USERNAME=administrator
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
+python manage.py runserver 0.0.0.0:8000
